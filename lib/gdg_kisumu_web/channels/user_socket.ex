b53e47971265dea4af5b7aad7abfb4e7aec82111
sigu <sigumagwa@gmail.com>
2017-07-29 14:00:15 +0300

    Add: configure for heroku deployment

--- lib/gdg_kisumu_web/channels/user_socket.ex
+++ lib/gdg_kisumu_web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule GdgKisumuWeb.UserSocket do
   # channel "room:*", GdgKisumuWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+  timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
