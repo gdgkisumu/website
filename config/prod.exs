06ee2da2d701d7c1a7491aae75674d6226519dab
sigu <sigumagwa@gmail.com>
2017-07-30 11:52:36 +0300

    Fix: fix typo

--- config/prod.exs
+++ config/prod.exs
@@ -20,7 +20,7 @@ config :gdg_kisumu, GdgKisumuWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
-config :gdg_kisumu, GdgKisumub.Repo,
+config :gdg_kisumu, GdgKisumu.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
